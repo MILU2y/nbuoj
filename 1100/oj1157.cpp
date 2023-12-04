46a9bbb68d0530be3c76f97bad4c5dcce79e5547
MILU2y <151505870+MILU2y@users.noreply.github.com>
2023-12-04 23:09:46 +0800

    Update oj1157.cpp

--- 1100/oj1157.cpp
+++ 1100/oj1157.cpp
@@ -1,1 +1,19 @@
 // 这是文件 oj1157.cpp 的内容
+#include <stdio.h>
+int main()
+{
+	int s[10],i;
+	int max,maxindex,min,minindex;
+	for(i=0;i<10;i++)
+	scanf("%d",&s[i]);
+	max=s[0]; min=s[0];
+	for(i=1;i<10;i++)
+	if(s[i]>max)
+	{max=s[i];maxindex=i;}
+	printf("%d% d\n",max,maxindex);
+	for(i=1;i<10;i++)
+	if(s[i]<min)
+	{min=s[i];minindex=i;}
+    printf("%d %d\n",min,minindex);
+	return 0;
+}
