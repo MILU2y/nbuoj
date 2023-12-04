4c03c93b3346d7921e58a23a700a867834349484
MILU2y <151505870+MILU2y@users.noreply.github.com>
2023-12-04 23:08:50 +0800

    Update oj1075.cpp

--- 1000/oj1075.cpp
+++ 1000/oj1075.cpp
@@ -1,1 +1,25 @@
 // 这是文件 oj1075.cpp 的内容
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int s[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+	int a[12]={31,29,31,30,31,30,31,31,30,31,30,31};
+	int year,day,month,d;
+	int i;
+	scanf("%d%d",&year,&day);
+	if(year%4==0&&year%100!=0||year%400==0)
+	{
+		for(i=0;day>=s[i];i++)
+	day=day-a[i];
+	month=i+1;
+	printf("%d-%d-%d",year,month,day);
+	}
+	else
+	{for(i=0;day>=s[i];i++)
+	day=day-s[i];
+	month=i+1;
+	printf("%d-%d-%d",year,month,day);
+	}
+	return 0;
+}
