42061fd8b2f117d96dcab63a8531de48d24febe2
MILU2y <151505870+MILU2y@users.noreply.github.com>
2023-12-05 11:05:25 +0800

    Update oj1167.cpp

--- 1100/oj1167.cpp
+++ 1100/oj1167.cpp
@@ -1,1 +1,17 @@
 // 这是文件 oj1167.cpp 的内容
+#include<stdio.h>
+int main()
+{
+	double sum=0.00, RMB[10]={0.1,0.2,0.5,1,2,5,10,20,50,100};
+	int index, num;
+	for(;;)
+	{
+	scanf("%d%d",&index,&num);
+	if(index>0&&num>0)
+    {index-=1;
+    sum+=num*RMB[index];}
+	else break;
+	}
+	printf("%.2lf\n",sum);
+	return 0;
+}
