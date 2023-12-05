bd37c015f76250a4a814a63aff8bad819c73166d
MILU2y <151505870+MILU2y@users.noreply.github.com>
2023-12-05 11:06:08 +0800

    Update oj1172.cpp

--- 1100/oj1172.cpp
+++ 1100/oj1172.cpp
@@ -1,1 +1,18 @@
 // 这是文件 oj1172.cpp 的内容
+#include<stdio.h>
+#define N 1000
+int main()
+{
+	int ten, eight[1000];
+    int i,j;
+	scanf("%d",&ten);
+	for(i=0;ten!=0;i++)
+    {
+		eight[i]=ten%8;
+		ten=ten/8;
+	}
+    for(j=i-1;j>=0;j--)
+	printf("%d ",eight[j]);
+	printf("\n");
+	return 0;
+}
