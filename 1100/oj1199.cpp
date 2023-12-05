6236322403d229192397b30a5e4370d26df3b74d
MILU2y <151505870+MILU2y@users.noreply.github.com>
2023-12-05 11:00:01 +0800

    Update oj1199.cpp

--- 1100/oj1199.cpp
+++ 1100/oj1199.cpp
@@ -1,1 +1,24 @@
 // 这是文件 oj1199.cpp 的内容
+#include<stdio.h>
+#define N 1000
+int main()
+{
+	char str[N];
+	gets(str);
+	int digital=0,character=0;
+	int i;
+	for(i=0;str[i]!='\0';i++)
+	{
+		if('0'<=str[i]&&str[i]<='9')
+		digital=1;
+		if('a'<=str[i]&&str[i]<='z'||'A'<=str[i]&&str[i]<='Z')
+		character=1;
+	}
+	if(digital==1&&character==0)
+	printf("digit\n");
+    if(digital==0&&character==1)
+	printf("character\n");
+    if(digital==1&&character==1)
+    printf("mixed\n");
+	return 0;
+}
