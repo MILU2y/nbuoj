0d7152b92c42e7416db88202b61a8500ec928018
MILU2y <151505870+MILU2y@users.noreply.github.com>
2023-12-05 11:03:53 +0800

    Update oj1054.cpp

--- 1000/oj1054.cpp
+++ 1000/oj1054.cpp
@@ -1,1 +1,16 @@
 // 这是文件 oj1054.cpp 的内容
+#include <stdio.h>
+#define N 1000
+int main()
+{
+	char str[N];
+	gets(str);
+	int i ,j;
+	int flag=0;
+	for(i=0,j=1;str[j]!='\0';i++,j++)
+	if(str[i]==str[j])
+	{printf("%c\n",str[i]);flag=1;break;}
+	if(flag==0)
+	printf("No\n");
+	return 0;
+}
