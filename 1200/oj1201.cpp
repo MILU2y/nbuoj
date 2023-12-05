0596c7b9fb7a11b4df1c7e18353ac9de2d1d841a
MILU2y <151505870+MILU2y@users.noreply.github.com>
2023-12-05 11:00:38 +0800

    Update oj1201.cpp

--- 1200/oj1201.cpp
+++ 1200/oj1201.cpp
@@ -1,1 +1,20 @@
 // 这是文件 oj1201.cpp 的内容
+#include <stdio.h>
+#include <string.h>
+#define N 100
+int main() {
+    char strings[5][N];
+    char shortest[N];
+    int i;
+    for (i = 0; i < 5; i++) {
+        gets(strings[i]);
+    }
+    strcpy(shortest, strings[0]);
+    for (i = 1; i < 5; i++) {
+        if (strlen(strings[i]) < strlen(shortest)) 
+        strcpy(shortest, strings[i]);
+        
+    }
+    printf("%s\n", shortest);
+    return 0;
+}
