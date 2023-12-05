7f0c2218a8c3f0f6c3b652b0e8b7300d572be305
MILU2y <151505870+MILU2y@users.noreply.github.com>
2023-12-05 10:59:04 +0800

    Update oj1315.cpp

--- 1300/oj1315.cpp
+++ 1300/oj1315.cpp
@@ -1,1 +1,19 @@
 // 这是文件 oj1315.cpp 的内容
+#include<stdio.h>
+#define N 1001
+int main()
+{
+	char str[N];
+	gets(str);
+	int i,num=0;
+	for(i=0;str[i]!='\0';i++)
+	{
+		if('0'<=str[i]&&str[i]<='9')
+		{num++;
+		for(;'0'<=str[i]&&str[i]<='9';)
+		i++;
+		}
+	}
+	printf("%d\n",num);
+	return 0;
+}
