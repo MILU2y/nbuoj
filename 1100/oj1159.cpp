a9962a184489287db12d1770555798503e5cf92a
MILU2y <151505870+MILU2y@users.noreply.github.com>
2023-12-05 11:01:45 +0800

    Update oj1159.cpp

--- 1100/oj1159.cpp
+++ 1100/oj1159.cpp
@@ -1,1 +1,26 @@
 // 这是文件 oj1159.cpp 的内容
+#include<stdio.h>
+#define N 10000
+int main()
+{
+	char str[N];
+	gets(str);
+	int num[26]={0};
+	int i,digital;
+	char letter;
+	for(i=0;str[i]!='\0';i++)
+	{
+	if('a'<=str[i]&&str[i]<='z')
+	{digital=str[i]-'a';
+	num[digital]+=1;}
+    if('A'<=str[i]&&str[i]<='Z')
+	{digital=str[i]-'A';
+	num[digital]+=1;}
+	}
+	for(i=0;i<26;i++)
+	{if(num[i]!=0)
+	{letter='A'+i;
+	printf("'%c':%d\n",letter,num[i]);}
+	}
+	return 0;
+}
