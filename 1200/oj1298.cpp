65dd5eb26e99e80ee52f793c3d42e7bc06cb8b54
MILU2y <151505870+MILU2y@users.noreply.github.com>
2023-12-05 11:05:00 +0800

    Update oj1298.cpp

--- 1200/oj1298.cpp
+++ 1200/oj1298.cpp
@@ -1,1 +1,18 @@
 // 这是文件 oj1298.cpp 的内容
+#include <stdio.h>
+int main()
+{
+	int a[5][5];
+	int i , j;
+	for(i=0;i<5;i++)
+	for(j=0;j<5;j++)
+	scanf("%d",&a[i][j]);
+	for(i=0;i<5;i++)
+    for(j=i+1;j<5;j++)
+	a[i][j]=0;
+	for(i=0;i<5;i++)
+	{for(j=0;j<5;j++)
+	printf("%4d",a[i][j]);
+	printf("\n");}
+	return 0;
+}
