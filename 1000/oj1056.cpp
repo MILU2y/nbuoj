71f37a63ee46c51cc9354f2610a724dbd34f94a1
MILU2y <151505870+MILU2y@users.noreply.github.com>
2023-12-05 11:04:21 +0800

    Update oj1056.cpp

--- 1000/oj1056.cpp
+++ 1000/oj1056.cpp
@@ -1,1 +1,16 @@
 // 这是文件 oj1056.cpp 的内容
+#include <stdio.h>
+#define N 1000
+int main()
+{
+	char str[N];
+	gets(str);
+	char key;
+	key=getchar();
+	int i=0, count=0;
+	for(i;str[i]!='\0';i++)
+	if(key==str[i])
+	count++;
+    printf("%d\n",count);
+	return 0;
+}
