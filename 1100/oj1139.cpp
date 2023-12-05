17f9695cfacbea48f1bb5ce6285f7659c564de7e
MILU2y <151505870+MILU2y@users.noreply.github.com>
2023-12-05 11:03:24 +0800

    Update oj1139.cpp

--- 1100/oj1139.cpp
+++ 1100/oj1139.cpp
@@ -1,1 +1,19 @@
 // 这是文件 oj1139.cpp 的内容
+#include<stdio.h>
+int main()
+{
+	char str[9];
+	gets(str);
+	int i;
+	for(i=0;i<9;i++)
+    {
+		if('a'<=str[i]&&str[i]<='v'||'A'<=str[i]&&str[i]<='V')
+		str[i]+=4;
+		if('w'<str[i]&&str[i]<='z'||'W'<=str[i]&&str[i]<='Z')
+        str[i]=str[i]+4-26;
+	}
+	for(i=0;i<9;i++)
+	printf("%c",str[i]);
+	printf("\n");
+	return 0;
+}
